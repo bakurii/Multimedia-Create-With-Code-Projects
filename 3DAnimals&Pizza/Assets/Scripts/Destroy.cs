f695a9743a7ed9226825865a5b990c31f1419d0f
Riina Backman <gr188369@gradia.fi>
2020-04-14 09:50:29 +0300

    Initial commit

---
+++ 3DAnimals&Pizza/Assets/Scripts/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float topBound = 30f;
+    private float lowerBound = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
