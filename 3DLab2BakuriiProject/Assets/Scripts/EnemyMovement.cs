1920ac7f785bed1fe3c5915a9d7489f9568280d6
Riina Backman <gr188369@gradia.fi>
2020-04-23 09:06:56 +0300

    Updates
    Challenge 4++

---
+++ 3DLab2BakuriiProject/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speed2 = 30f;
+
+    public Rigidbody enemyRb;
+    public Rigidbody bossEnemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossEnemyRb = GetComponent<Rigidbody>();
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce(Vector3.forward * -speed);
+        bossEnemyRb.AddForce(Vector3.forward * -speed2);
+    }
+}
---
+++ 3DLab2BakuriiProject/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject yellowUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yellowUp = GetComponent<GameObject>();
+        float randomZ = Random.Range(-7, 7);
+        float randomX = Random.Range(-7, 7);
+        Vector3 position = new Vector3(randomZ, 0, randomX);
+        yellowUp.transform.position = position;
+        Instantiate(yellowUp);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
